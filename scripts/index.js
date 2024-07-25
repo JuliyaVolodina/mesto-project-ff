bf8c16dff28917d994276b4198133a9844df616e
Yuliya Volodina <fortress2017@yandex.ru>
2024-07-26 00:35:58 +0500

    мой первоначальный коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+function addCard(element, deleteItem) {
+    const placesList = document.querySelector('.places__list');
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click',function() {
+        deleteItem(cardElement);
+    });
+    placesList.append(cardElement);
+}
+  
+function deleteItem(cardElement) {
+    return cardElement.remove();
+}
 
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for(let i = 0; i < initialCards.length; i++) {
+    addCard(initialCards[i], deleteItem);
+}
+  
